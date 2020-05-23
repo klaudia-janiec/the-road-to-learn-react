b8563449eefb1e60f9be9c2ecbfa1fce49cfd1af
Klaudia Janiec <klaudia.janiec@gmail.com>
2020-05-23 21:50:12 +0200

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const list = [
@@ -17,25 +17,68 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div className="App">
-    { list.map(item =>
-      <div key={item.objectID}>
-        <div>
-          <span>
-            <a href={item.url}>
-              {item.title}
-            </a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div> 
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+      <form>
+        <input
+          type="text"
+          onChange={this.onSearchChange}
+        />
+      </form>
+
+      { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+        <div key={item.objectID}>
+          <div>
+            <span>
+              <a href={item.url}>
+                {item.title}
+              </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div> 
+        </div>
+      )}
       </div>
-    )}
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
