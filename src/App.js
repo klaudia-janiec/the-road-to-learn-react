d1df08d3058ae4ea892fe72e9431e6b4567d01eb
Klaudia Janiec <klaudia.janiec@gmail.com>
2020-05-31 18:51:38 +0200

    User stateless functional components

--- src/App.js
+++ src/App.js
@@ -49,39 +49,58 @@ class App extends Component {
 
     return (
       <div className="App">
-      <form>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        />
-      </form>
+      <Search
+        value={searchTerm}
+        onChange={this.onSearchChange}
+      >
+        Search
+      </Search>
 
-      { list.filter(isSearched(searchTerm)).map(item =>
-        <div key={item.objectID}>
-          <div>
-            <span>
-              <a href={item.url}>
-                {item.title}
-              </a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div> 
-        </div>
-      )}
+      <Table
+        list={list}
+        pattern={searchTerm}
+        onDismiss={this.onDismiss}
+      />
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    { list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+        <Button onClick={() => onDismiss(item.objectID)}>
+          Dismiss
+        </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({ onClick, className = '', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
