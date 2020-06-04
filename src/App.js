73cc0c4ceff97a0a33f8c1e6c8a83e189e75a81d
Klaudia Janiec <klaudia.janiec@gmail.com>
2020-06-04 20:15:50 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 const list = [
   {
     title: 'React',
-    url: 'https://facebook.github.io/react/', author: 'Jordan Walke',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
     title: 'Redux',
-    url: 'https://github.com/reactjs/redux', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
     points: 5,
     objectID: 1,
   },
